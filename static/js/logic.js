5ac10a94130529da16c280d39b64ef6f2fcd09fe
Bill Johnson <wpj174@gmail.com>
2022-01-18 09:51:00 -0500

    Tectonic Plates
    Tectonic plates layer added.

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,11 +5,85 @@ var defaultMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
+// grayscale layer
+var grayscale =  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+});
+
+// terrain layer 1
+var terrain1 = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+});
+
+// terrain layer 2
+var terrain2 = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+});
+
+// topo layer
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+// make basemabes object
+let basemaps = {
+  Topographic: topo,
+  Terrain1: terrain1,
+  Terrain2: terrain2,
+  GrayScale: grayscale,
+  Default: defaultMap
+};
+
 var myMap = L.map("map", {
-  center: [34.026810, -84.602240],
-  zoom: 3
+  center: [50, -120],
+  zoom: 3,
+  layers: [defaultMap, grayscale, terrain1, terrain2, topo]
 });
 
 // Adding a tile layer (the background map image) to our map:
 // We use the addTo() method to add objects to our map.
 defaultMap.addTo(myMap);
+
+// tectonic plate data
+// variable to hold the tectonic plate layer
+var tectonic = new L.layerGroup();
+
+// call api to get tectonic plate data
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json")
+  .then(function(plateData){
+    //console.log(plateData);
+
+    // load date w/ geojson and add to tectonic layer
+    L.geoJson(plateData,{
+      // add styling to make visible
+      color: "brown",
+      weight: 1
+    }).addTo(tectonic);
+  });
+
+  // add tectonic plate layer to map
+  tectonic.addTo(myMap);
+
+  // add the tectonic overlay
+  var overlays = {
+    "Tectonic Plates": tectonic
+  };
+
+// add the layer control
+L.control
+  .layers(basemaps, overlays)
+  .addTo(myMap);
+
